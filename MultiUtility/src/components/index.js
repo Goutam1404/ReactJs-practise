6c57f2284a971b68a92dd501db3781125ca12a90
Goutam1404 <goutamkumar9093@gmail.com>
2026-02-27 17:40:09 +0530

    Fix: correctly ignore all .env files

--- MultiUtility/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import TodoForm from "./TodoForm.jsx";
-import TodoDisplay from "./TodoDisplay.jsx";
-import NavBar from "./NavBar.jsx";
-import NoteForm from "./NoteForm.jsx";
-import NotesDisplay from "./NotesDisplay.jsx";
-export { TodoForm, TodoDisplay, NoteForm, NavBar, NotesDisplay };
--- MultiUtility/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import NotePage from "./NotePage.jsx"
-import ClockPage from "./ClockPage.jsx"
-import TodoPage from "./TodoPage.jsx"
-import HomePage from "./HomePage.jsx"
-export {NotePage, ClockPage, TodoPage, HomePage}
